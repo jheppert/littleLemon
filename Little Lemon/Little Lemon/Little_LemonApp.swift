87ce9b80515f9d8c38909010d4632e968058061a
Jeff Heppert <jeffheppert@gmail.com>
2023-11-25 09:29:12 -0800

    first commit

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Jeff Heppert on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
